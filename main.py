01776ef5c7b3df9de91a08ba1ac25a951be064ec
kevinzeroCode <410410681@gms.tku.edu.tw>
2023-12-03 09:09:57 +0800

    plus gamma function

--- main.py
+++ main.py
@@ -32,16 +32,22 @@ class ImageProcessor:
         self.button_binary.pack()
 
         self.threshold_var = tk.IntVar()
-        self.threshold_var.set(127)# 初始閾值
+        self.threshold_var.set(127)
         self.threshold_slider = Scale(root, label="閾值", orient=tk.HORIZONTAL, from_=0, to=255,
                                       variable=self.threshold_var, command=self.produce_image)
         self.threshold_slider.pack()
 
         self.rotation_var = tk.IntVar()
-        self.rotation_var.set(0)  # 初始旋轉角度
+        self.rotation_var.set(0)
         self.rotation_slider = Scale(root, label="旋轉角度", orient=tk.HORIZONTAL, from_=0, to=360,
                                      variable=self.rotation_var, command=self.produce_image)
         self.rotation_slider.pack()
+
+        self.gamma_var = tk.DoubleVar()
+        self.gamma_var.set(1.0)
+        self.gamma_slider = Scale(root, label="Gamma", orient=tk.HORIZONTAL, from_=0.1, to=5.0,
+                                  resolution=0.1, variable=self.gamma_var, command=self.produce_image)
+        self.gamma_slider.pack()
 
         self.button_extract = tk.Button(root, text="區域放大", command=self.extract_region)
         self.button_extract.pack()
@@ -62,12 +68,12 @@ class ImageProcessor:
     def add_image_effect(self, effect):
         self.image_effect.append(effect)
         self.produce_image()
-        
-    def produce_image(self, _=None ,image = None):
+
+    def produce_image(self, _=None, image=None):
         if self.image is not None:
             if image is not None:
                 self.retrieve_image = image
-            self.processed_image = self.retrieve_image
+            self.processed_image = self.retrieve_image.copy()  # Make a copy to avoid modifying the original
             if collections.Counter(self.image_effect)["flip"] % 2:
                 self.flip_image()
             if collections.Counter(self.image_effect)["gray"] % 2:
@@ -77,10 +83,9 @@ class ImageProcessor:
             if collections.Counter(self.image_effect)["binary"] % 2:
                 self.apply_binary()
             self.rotate_image()
-            
-            self.display_image("processed") # 顯示處理後的圖像
-                    
-                    
+            self.adjust_gamma()
+            self.display_image("processed")
+
     def open_image(self):
         file_path = filedialog.askopenfilename()
         if file_path:
@@ -88,71 +93,63 @@ class ImageProcessor:
             self.image = cv2.imread(file_path)
         if self.image is None:
             print("不支援此圖片檔名或其他錯誤")
-            return 
-        self.processed_image = self.image
-        self.retrieve_image = self.image
+            return
+        self.processed_image = self.image.copy()
+        self.retrieve_image = self.image.copy()
         self.resized_image = self.resize_image(self.image)
-        self.display_image("original")  # 顯示原圖
+        self.display_image("original")
         self.image_effect = []
-
 
     def resize_image(self, image):
         if image is not None:
-            # 計算等比例縮放後的高度
             height = int(image.shape[0] * (self.show_width / image.shape[1]))
             return cv2.resize(image, (self.show_width, height))
 
     def flip_image(self):
-        #水平翻轉
-        self.processed_image = cv2.flip(self.processed_image, 1)  # 1表示水平翻轉
+        self.processed_image = cv2.flip(self.processed_image, 1)
 
     def convert_to_gray(self):
-        #灰階圖像
         self.processed_image = cv2.cvtColor(self.processed_image, cv2.COLOR_BGR2GRAY)
 
     def apply_negative(self):
-        #負片
         self.processed_image = 255 - self.processed_image
 
     def apply_binary(self, event=None):
-        #二值化
         threshold_value = self.threshold_var.get()
         _, self.processed_image = cv2.threshold(self.processed_image, threshold_value, 255, cv2.THRESH_BINARY)
 
     def rotate_image(self, event=None):
-        # 旋轉
         rotation_angle = self.rotation_var.get()
-        if len(self.processed_image.shape) == 2:    # 灰階圖像
+        if len(self.processed_image.shape) == 2:
             rows, cols = self.processed_image.shape
         else:
             rows, cols, _ = self.processed_image.shape
         rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), rotation_angle, 1)
-        self.processed_image = cv2.warpAffine(self.processed_image, rotation_matrix, (cols, rows)) 
+        self.processed_image = cv2.warpAffine(self.processed_image, rotation_matrix, (cols, rows))
+
+    def adjust_gamma(self):
+        gamma_value = self.gamma_var.get()
+        gamma_corrected = np.power(self.processed_image / 255.0, gamma_value)
+        self.processed_image = np.uint8(255 * gamma_corrected)
 
     def extract_region(self):
         if self.resized_image is not None:
-            # 開始選取矩形區域
             self.canvas_original.bind("<ButtonPress-1>", self.on_press)
             self.canvas_original.bind("<B1-Motion>", self.on_drag)
             self.canvas_original.bind("<ButtonRelease-1>", self.on_release)
 
     def on_press(self, event):
-        # 記錄起點位置
         self.start_x = self.canvas_original.canvasx(event.x)
         self.start_y = self.canvas_original.canvasy(event.y)
 
-        # 刪除先前的矩形
         if self.selection_rectangle:
             self.canvas_original.delete(self.selection_rectangle)
 
-        # 建立新的矩形
         self.selection_rectangle = self.canvas_original.create_rectangle(self.start_x, self.start_y, self.start_x, self.start_y, outline="red")
 
     def on_drag(self, event):
         cur_x = self.canvas_original.canvasx(event.x)
         cur_y = self.canvas_original.canvasy(event.y)
-
-        # 更新矩形的座標
         self.canvas_original.coords(self.selection_rectangle, self.start_x, self.start_y, cur_x, cur_y)
 
     def on_release(self, event):
@@ -160,20 +157,17 @@ class ImageProcessor:
         end_y = self.canvas_original.canvasy(event.y)
 
         t = self.image.shape[1] / self.show_width
-        
-        # 截取選區內的圖像
+
         x1 = min(self.start_x, end_x) * t
         y1 = min(self.start_y, end_y) * t
         x2 = max(self.start_x, end_x) * t
         y2 = max(self.start_y, end_y) * t
-        
-        self.produce_image(image = self.image[int(y1):int(y2), int(x1):int(x2)])
-        
-        # 解綁事件，避免重複觸發
+
+        self.produce_image(image=self.image[int(y1):int(y2), int(x1):int(x2)])
+
         self.canvas_original.unbind("<ButtonPress-1>")
         self.canvas_original.unbind("<B1-Motion>")
         self.canvas_original.unbind("<ButtonRelease-1>")
-
 
     def save_image(self):
         if self.processed_image is not None:
@@ -194,8 +188,7 @@ class ImageProcessor:
 
         elif mode == "processed" and self.processed_image is not None:
             self.resized_processed_image = self.resize_image(self.processed_image)
-            print(self.resized_processed_image.shape)
-            if len(self.resized_processed_image.shape) == 2:  # 灰階圖像
+            if len(self.resized_processed_image.shape) == 2:
                 image_rgb = cv2.cvtColor(self.resized_processed_image, cv2.COLOR_GRAY2RGB)
             else:
                 image_rgb = cv2.cvtColor(self.resized_processed_image, cv2.COLOR_BGR2RGB)
